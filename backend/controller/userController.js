8c6a21b8cdfeb3b135591e883082f7479351562a
Isha_Projects <duttaisha961@gmail.com>
2026-04-28 20:31:48 +0530

    versel deploy trying

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -76,7 +76,11 @@ res.cookie("token", token, {
 };
 
 export const logout = async (req, res) => {
-  res.clearCookie("token"); 
+  res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
   res.status(200).json({
     success: true,
     message: "Logged out successfully"
